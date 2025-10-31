e0eda14a3ad72087508e7747e5e6732f2a811f6b
Jayden <108575002+jaydensrc@users.noreply.github.com>
2025-10-31 16:05:48 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 6153709 then
+    --loadstring(game:HttpGet("http://127.0.0.1:5500/NuclearBlastTestingFacility.lua"))()
+elseif game.PlaceId == 7569392700 then
+    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/e50d386fba774443a5f4391fd84b586f"))()
+    --loadstring(game:HttpGet("http://127.0.0.1:5500/SquidGameRoleplay.lua"))()
+else
+    -- not supported
+end
